dda62f89eb9d8184104f57e6a0f4254843cce42d
mdfazal <fazalmd964@gmail.com>
2024-03-16 02:32:17 +0530

    variables

--- Javascript/helloworld.js
+++ Javascript/helloworld.js
@@ -18,3 +18,12 @@ let humanDogratio = 7
 let myDogage = myAge * humanDogratio
 
 console.log(myDogage)
+
+let bonuspoint = 50
+
+bonuspoint = bonuspoint + 50
+console.log(bonuspoint)
+bonuspoint = bonuspoint - 75
+console.log(bonuspoint)
+bonuspoint = bonuspoint + 50
+console.log(bonuspoint)
