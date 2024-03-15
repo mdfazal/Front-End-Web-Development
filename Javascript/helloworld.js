7d826c6e351b6e9ca05d831c228ce8100cb2afb5
mdfazal <fazalmd964@gmail.com>
2024-03-16 02:21:42 +0530

    variables

---
+++ Javascript/helloworld.js
@@ -0,0 +1,20 @@
+// document.getElementById("count-el").innerText = 5
+
+// to comment a whole bunch of lines press ctrl+k+c or cmd+k+c
+
+let firstbatch = 5
+let secondbatch = 7
+
+let count = firstbatch + secondbatch
+
+console.log(count)
+
+let myAge = 26
+
+console.log(myAge)
+
+let humanDogratio = 7
+
+let myDogage = myAge * humanDogratio
+
+console.log(myDogage)
